38a81c182b79e9c59558f005a46628ec82b6e610
Daniela Segura <danielasegura1705@gmail.com>
2024-06-25 08:07:09 -0500

    first commit

--- app/src/main/java/com/example/productapp/HomeActivity.java
+++ app/src/main/java/com/example/productapp/HomeActivity.java
@@ -1,17 +1,34 @@
 package com.example.productapp;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
     private Button addProductButton, viewProductsButton, logoutButton;
     private SharedPreferences sharedPreferences;
 
+    DrawerLayout drawerLayout;
+    ImageButton buttonDrawerToggle;
+    NavigationView navigationView;
+    ImageView logoImage;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +40,47 @@ public class HomeActivity extends AppCompatActivity {
         viewProductsButton = findViewById(R.id.viewProductsButton);
         logoutButton = findViewById(R.id.logoutButton);
 
-        addProductButton.setOnClickListener(v -> startActivity(new Intent(HomeActivity.this, AddProductActivity.class)));
-        viewProductsButton.setOnClickListener(v -> startActivity(new Intent(HomeActivity.this, ViewProductsActivity.class)));
+        drawerLayout = findViewById(R.id.drawerLayout);
+        buttonDrawerToggle = findViewById(R.id.buttonDrawerToggle);
+        navigationView = findViewById(R.id.navigationView);
+
+        addProductButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AddProductActivity.class)));
+        viewProductsButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ViewProductsActivity.class)));
         logoutButton.setOnClickListener(v -> logoutUser());
+
+        buttonDrawerToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.open();
+            }
+        });
+
+        View headerView = navigationView.getHeaderView(0);
+        logoImage = headerView.findViewById(R.id.logoImage);
+        TextView textUsername = headerView.findViewById(R.id.textUsername);
+
+        logoImage.setOnClickListener (new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, textUsername.getText(), Toast.LENGTH_SHORT).show();
+            }
+            });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int iteamId = item.getItemId();
+
+                if (iteamId == R.id.navProduct){
+                    Toast.makeText(MainActivity.this, "Product clicked", Toast.LENGTH_SHORT).show();
+                }
+
+
+                drawerLayout.close();
+
+                return false;
+            }
+        });
     }
 
     private void logoutUser() {
@@ -33,7 +88,10 @@ public class HomeActivity extends AppCompatActivity {
         editor.putBoolean("isLoggedIn", false);
         editor.putString("email", null);
         editor.apply();
-        startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
         finish();
     }
+
+
+
 }
